b8cdd89252e98dfeec78230a37fc66d529ec8f55
FrontendFlair <hamaadafzal404@gmail.com>
2024-12-31 07:38:14 -0800

    commit

--- components/Home/WhoWeAre.jsx
+++ components/Home/WhoWeAre.jsx
@@ -3,33 +3,30 @@ import SectionHeader from "../common/SectionHeader";
 import Image from "next/image";
 
 const WhoWeAre = () => {
-  const services = [
+  const highlights = [
     {
-      title: "Web Design Agency",
+      title: "Customer-Centric Approach",
       description:
-        "As a leading web design and digital agency, our design and development specialists take all measures to ensure your website works like no other.",
-      image: "/coding.png",
+        "Our priority is understanding your needs and delivering tailor-made solutions that align with your goals.",
+      image: "/target.png",
     },
     {
-      title: "App Development",
+      title: "Innovative Solutions",
       description:
-        "We specialize in creating innovative mobile apps that bring your ideas to life and drive business success.",
-      image: "/app-development.png",
+        "We thrive on innovation, creating groundbreaking ideas that drive success and growth.",
+      image: "/project-management.png",
     },
     {
-      title: "Digital Marketing",
+      title: "Global Reach",
       description:
-        "Our team uses cutting-edge marketing strategies to boost your brand’s visibility, engagement, and conversions.",
-      image: "/social-media-marketing.png",
+        "With clients across the globe, we bring an international perspective to every project.",
+      image: "/global.png",
     },
   ];
 
   return (
     <div className="container-section">
-      <SectionHeader
-        title="Who we are"
-        text="Your partners for digital success"
-      />
+      <SectionHeader title="Who we are" text="Discover what makes us unique" />
       <div className="max-w-3xl ml-auto pb-8 md:pb-16">
         <p className="text-base md:text-lg text-gray-100 font-light text-balance">
           We’re a team of expert designers, web developers, and marketers who’ve
@@ -39,23 +36,23 @@ const WhoWeAre = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {services.map((service, index) => (
+        {highlights.map((highlight, index) => (
           <div
             key={index}
-            className="bg-gradient-to-br hover:bg-gradient-to-tl space-y-4 from-blue-800/50 via-purple-700/50 to-pink-600/50 rounded-xl p-6 shadow-lg hover:shadow-xl hover:-translate-y-2 transition-all duration-300"
+            className="bg-gradient-to-br hover:bg-gradient-to-tl space-y-3 from-blue-800/50 via-purple-700/50 to-pink-600/50 rounded-xl p-6 shadow-lg hover:shadow-xl hover:-translate-y-2 transition-all duration-300"
           >
             <Image
               width={500}
               height={500}
-              src={service.image}
-              alt={service.title}
+              src={highlight.image}
+              alt={highlight.title}
               className="w-20 h-20 object-contain"
             />
             <h2 className="text-xl font-semibold text-main-300">
-              {service.title}
+              {highlight.title}
             </h2>
-            <p className="mt-4 text-gray-200 leading-relaxed">
-              {service.description}
+            <p className="text-gray-200 leading-relaxed">
+              {highlight.description}
             </p>
           </div>
         ))}
